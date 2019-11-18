9e400a744cd4d1e5aa8c0df2c8b580679c88b485
SophiaYST <sophia12332111@gmail.com>
2019-11-17 20:03:10 -0500

    commit

--- app/src/main/java/com/example/finalproject/MainRecipeActivity.java
+++ app/src/main/java/com/example/finalproject/MainRecipeActivity.java
@@ -7,6 +7,7 @@ import android.app.AlertDialog;
 import android.app.AppComponentFactory;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,17 +17,38 @@ import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainRecipeActivity extends AppCompatActivity {
     private EditText searchEditText;
     private Button btnSearch, btnFavourite;
     private ListView liistView;
     protected static final String Activity_NAME = "RecipeSearchActivity";
-    private String app_key = "";
+    private String app_key = "fdfc2f97466caa0f5b142bc3b913c366";
+    private static String food;
+    private List<MyRecipe> newBeanList = new ArrayList<>();
+    private String jsonUrl = "https://www.food2fork.com/api/search?key="+ app_key+ "&q=" + food+ "%20" + breast；
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
         ListView listView = (ListView) findViewById(R.id.recipe_list);
+        searchEditText = (EditText) findViewById(R.id.recipe_search);
+        btnSearch = (Button) findViewById(R.id.recipe_searchButton);
+        btnFavourite = (Button) findViewById(R.id.btn_favourite);
+
+        btnSearch.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                food = searchEditText.getText().toString();
+                if(food!=null && !food.isEmpty()){
+                    new RecipeAsyncTask().execute(jsonUrl);
+                }else{
+                    toastMsg(getString(R.string.prompt_to_enter));
+                }
+            }
+        });
 
 //        Button button = (Button) findViewById(R.id.button);
 //        button.setOnClickListener(new View.OnClickListener(){
@@ -35,12 +57,6 @@ public class MainRecipeActivity extends AppCompatActivity {
 //                openDialog();
 //            }
 //        });
-
-//
-//        Toast toast=Toast.makeText(getApplicationContext(),"Welcome!",Toast.LENGTH_SHORT);
-//        toast.setMargin(50,50);
-//        toast.show();
-//
 //        Button button2 = (Button) findViewById(R.id.button2);
 //        button2.setOnClickListener(new View.OnClickListener(){
 //            @Override
@@ -58,4 +74,20 @@ public class MainRecipeActivity extends AppCompatActivity {
         DialogClass dialog = new DialogClass();
         dialog.show(getSupportFragmentManager(),"dialog box");
     }
+
+    public void toastMsg(String msg){
+        Toast toast= Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_SHORT);
+        toast.setMargin(50,50);
+        toast.show();
+    }
+
+    private class RecipeAsyncTask extends AsyncTask<String, void, List<MyRecipe>>{
+
+
+        @Override
+        protected List<MyRecipe> doInBackground(String... strings) {
+            return null;
+        }
+    }
+
 }
