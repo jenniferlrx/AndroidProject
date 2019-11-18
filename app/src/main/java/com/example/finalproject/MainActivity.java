d655b5e3f789d6fc57d8b02a98dc02d93d673cd0
SteveHuangAC <huan0213@algonquinlive.com>
2019-11-17 19:24:30 -0500

    Steve V0.2 NOV 17 milestone 1 finished

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton imgBtnCar = (ImageButton) findViewById(R.id.imgBtn_car);
+        ImageButton imgBtnRecipe = (ImageButton) findViewById(R.id.imgBtn_recipe);
+        ImageButton imgBtnCurrency = (ImageButton) findViewById(R.id.imgBtn_currency);
+        ImageButton imgBtnNews = (ImageButton) findViewById(R.id.imgBtn_news);
+
+        imgBtnCar.setOnClickListener(v->{
+            startActivity(new Intent(MainActivity.this, ECCSFmain.class));
+        });
+
+        imgBtnNews.setOnClickListener(v->{
+            startActivity(new Intent(MainActivity.this, News_Activity_Main.class));
+        });
+
     }
 }
