6cc3e49b76c92f5da091f1eda65e6588932f99a9
SophiaYST <sophia12332111@gmail.com>
2019-11-19 20:15:11 -0500

    commit

---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.ImageButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton imgBtnCar = (ImageButton) findViewById(R.id.imgBtn_car);
+        ImageButton imgBtnRecipe = (ImageButton) findViewById(R.id.imgBtn_recipe);
+        ImageButton imgBtnCurrency = (ImageButton) findViewById(R.id.imgBtn_currency);
+        ImageButton imgBtnNews = (ImageButton) findViewById(R.id.imgBtn_news);
+
+        imgBtnCar.setOnClickListener(v->{
+            startActivity(new Intent(MainActivity.this, ECCSFmain.class));
+        });
+        imgBtnCurrency.setOnClickListener(v->{
+            startActivity(new Intent(MainActivity.this, CurrencyActivity.class));
+        });
+        imgBtnRecipe.setOnClickListener(v->{
+            startActivity(new Intent(MainActivity.this, RecipeSearchActivity.class));
+        });
+    }
+}
