35c3627e11366e535c21617b2d6ee03696b67e41
jennifer <jenniferlrx2@gmail.com>
2019-11-25 17:36:23 -0500

    show saved icon in fav page

--- app/src/main/java/com/example/finalproject/ECCSFfav.java
+++ app/src/main/java/com/example/finalproject/ECCSFfav.java
@@ -193,7 +193,7 @@ public class ECCSFfav extends AppCompatActivity {
             TextView title = oldView.findViewById(R.id.row_title);
             TextView address = oldView.findViewById(R.id.row_address);
             ImageView saved = oldView.findViewById(R.id.row_saved);
-            saved.setVisibility(View.GONE);
+//            saved.setVisibility(View.GONE);
 
             title.setText(favStations.get(position).getTitle());
             address.setText(favStations.get(position).getAddress());
